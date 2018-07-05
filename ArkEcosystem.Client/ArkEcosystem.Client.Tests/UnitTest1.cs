7e00d200ffd5d31832d4479b6026ed09bc71ccb0
Brian Faust <hello@brianfaust.me>
2018-07-05 18:41:08 +0300

    chore: setup solution for client & tests

---
+++ ArkEcosystem.Client/ArkEcosystem.Client.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArkEcosystem.Client.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
