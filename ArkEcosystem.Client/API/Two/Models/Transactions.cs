2619f3020547b5d1b2984272dd93f83de9ee4987
supaiku <supaiku@protonmail.com>
2018-07-21 19:24:50 +0200

    port transactions v2 api to new models with unit tests

--- ArkEcosystem.Client/API/Two/Models/Transactions.cs
+++ ArkEcosystem.Client/API/Two/Models/Transactions.cs
@@ -16,7 +16,7 @@ namespace ArkEcosystem.Client.API.Two.Models
         public string Recipient { get; set; }
         public string Signature { get; set; }
         public string VendorField { get; set; }
-        public string Asset { get; set; } // TODO
+        public dynamic Asset { get; set; } // TODO
         public long Confirmations { get; set; }
         public Timestamp Timestamp { get; set; }
     }
@@ -24,14 +24,23 @@ namespace ArkEcosystem.Client.API.Two.Models
     [JsonObject]
     public class TransactionTypes
     {
+        [JsonProperty("TRANSFER")]
         public byte Transfer { get; set; }
+        [JsonProperty("SECOND_SIGNATURE")]
         public byte SecondSignature { get; set; }
+        [JsonProperty("DELEGATE_REGISTRATION")]
         public byte DelegateRegistration { get; set; }
+        [JsonProperty("VOTE")]
         public byte Vote { get; set; }
+        [JsonProperty("MULTI_SIGNATURE")]
         public byte MultiSignature { get; set; }
+        [JsonProperty("IPFS")]
         public byte IPFS { get; set; }
+        [JsonProperty("TIMELOCK_TRANSFER")]
         public byte TimelockTransfer { get; set; }
+        [JsonProperty("MULTI_PAYMENT")]
         public byte MultiPayment { get; set; }
+        [JsonProperty("DELEGATE_RESIGNATION")]
         public byte DelegateResignation { get; set; }
     }
 }
