3b0c2e728eb47d6710d3e02481fb9250c520de88
supaiku <supaiku@protonmail.com>
2018-07-21 20:03:56 +0200

    port wallets v2 api to new models with unit tests

--- ArkEcosystem.Client/API/Two/Wallets.cs
+++ ArkEcosystem.Client/API/Two/Wallets.cs
@@ -23,8 +23,9 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
-using ArkEcosystem.Client.Helpers;
 using Newtonsoft.Json.Linq;
+using ArkEcosystem.Client.API.Two.Models;
+using ArkEcosystem.Client.Helpers;
 
 namespace ArkEcosystem.Client.API.Two
 {
@@ -37,110 +38,97 @@ namespace ArkEcosystem.Client.API.Two
             httpClient = client;
         }
 
-        public JObject All(Dictionary<string, string> parameters = null)
+        public Response<List<Wallet>> All(Dictionary<string, string> parameters = null)
         {
             return AllAsync(parameters).Result;
         }
 
-        public async Task<JObject> AllAsync(Dictionary<string, string> parameters = null)
+        public async Task<Response<List<Wallet>>> AllAsync(Dictionary<string, string> parameters = null)
         {
             var response = await httpClient.GetStringAsync("wallets");
-
-            return JObject.Parse(response);
+            return Two.ConvertResponse<List<Wallet>>(response);
         }
 
-        public JObject Show(string id)
+        public Response<Wallet> Show(string id)
         {
             return ShowAsync(id).Result;
         }
 
-        public async Task<JObject> ShowAsync(string id)
+        public async Task<Response<Wallet>> ShowAsync(string id)
         {
             var response = await httpClient.GetStringAsync(string.Format("wallets/{0}", id));
-
-            return JObject.Parse(response);
+            return Two.ConvertResponse<Wallet>(response);
         }
 
-        public JObject Transactions(string id, Dictionary<string, string> parameters = null)
+        public Response<List<Transaction>> Transactions(string id, Dictionary<string, string> parameters = null)
         {
             return TransactionsAsync(id, parameters).Result;
         }
 
-        public async Task<JObject> TransactionsAsync(string id, Dictionary<string, string> parameters = null)
+        public async Task<Response<List<Transaction>>> TransactionsAsync(string id, Dictionary<string, string> parameters = null)
         {
             var response = await httpClient.GetStringAsync(string.Format("wallets/{0}/transactions", id));
-
-            return JObject.Parse(response);
+            return Two.ConvertResponse<List<Transaction>>(response);
         }
 
-        public JObject SentTransactions(string id, Dictionary<string, string> parameters = null)
+        public Response<List<Transaction>> SentTransactions(string id, Dictionary<string, string> parameters = null)
         {
             return SentTransactionsAsync(id, parameters).Result;
         }
 
-        public async Task<JObject> SentTransactionsAsync(string id, Dictionary<string, string> parameters = null)
+        public async Task<Response<List<Transaction>>> SentTransactionsAsync(string id, Dictionary<string, string> parameters = null)
         {
             var uri = QueryBuilder.Build(string.Format("wallets/{0}/transactions/sent", id), parameters);
-
             var response = await httpClient.GetStringAsync(uri);
-
-            return JObject.Parse(response);
+            return Two.ConvertResponse<List<Transaction>>(response);
         }
 
-        public JObject ReceivedTransactions(string id, Dictionary<string, string> parameters = null)
+        public Response<List<Transaction>> ReceivedTransactions(string id, Dictionary<string, string> parameters = null)
         {
             return ReceivedTransactionsAsync(id, parameters).Result;
         }
 
-        public async Task<JObject> ReceivedTransactionsAsync(string id, Dictionary<string, string> parameters = null)
+        public async Task<Response<List<Transaction>>> ReceivedTransactionsAsync(string id, Dictionary<string, string> parameters = null)
         {
             var uri = QueryBuilder.Build(string.Format("wallets/{0}/transactions/received", id), parameters);
-
             var response = await httpClient.GetStringAsync(uri);
-
-            return JObject.Parse(response);
+            return Two.ConvertResponse<List<Transaction>>(response);
         }
 
-        public JObject Votes(string id, Dictionary<string, string> parameters = null)
+        public Response<List<Transaction>> Votes(string id, Dictionary<string, string> parameters = null)
         {
             return VotesAsync(id, parameters).Result;
         }
 
-        public async Task<JObject> VotesAsync(string id, Dictionary<string, string> parameters = null)
+        public async Task<Response<List<Transaction>>> VotesAsync(string id, Dictionary<string, string> parameters = null)
         {
             var uri = QueryBuilder.Build(string.Format("wallets/{0}/votes", id), parameters);
-
             var response = await httpClient.GetStringAsync(uri);
-
-            return JObject.Parse(response);
+            return Two.ConvertResponse<List<Transaction>>(response);
         }
 
-        public JObject Search(Dictionary<string, string> parameters)
+        public Response<List<Wallet>> Search(Dictionary<string, string> parameters)
         {
             return SearchAsync(parameters).Result;
         }
 
-        public async Task<JObject> SearchAsync(Dictionary<string, string> parameters)
+        public async Task<Response<List<Wallet>>> SearchAsync(Dictionary<string, string> parameters)
         {
             var formParams = new FormUrlEncodedContent(parameters);
-
             var response = await httpClient.PostAsync("wallets/search", formParams);
-
-            return JObject.Parse(await response.Content.ReadAsStringAsync());
+            return Two.ConvertResponse<List<Wallet>>(await response.Content.ReadAsStringAsync());
         }
 
-        public JObject Top(Dictionary<string, string> parameters = null)
+        public Response<List<Wallet>> Top(Dictionary<string, string> parameters = null)
         {
             return TopAsync(parameters).Result;
         }
 
-        public async Task<JObject> TopAsync(Dictionary<string, string> parameters = null)
+        public async Task<Response<List<Wallet>>> TopAsync(Dictionary<string, string> parameters = null)
         {
             var uri = QueryBuilder.Build("wallets/top", parameters);
-
             var response = await httpClient.GetStringAsync(uri);
-
-            return JObject.Parse(response);
+            return Two.ConvertResponse<List<Wallet>>(response);
         }
     }
 }
