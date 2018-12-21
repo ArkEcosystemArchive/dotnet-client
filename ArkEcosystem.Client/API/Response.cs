234b5e57d4e422017de0cdd284d09f7a8e722761
supaiku <supaiku@protonmail.com>
2018-12-21 15:22:08 +0100

    fix: add response status information

--- ArkEcosystem.Client/API/Response.cs
+++ ArkEcosystem.Client/API/Response.cs
@@ -10,5 +10,8 @@ namespace ArkEcosystem.Client.API
     {
         public Meta Meta { get; set; }
         public T Data { get; set; }
+        public int StatusCode { get; set; }
+        public string Error { get; set; }
+        public string Message { get; set; }
     }
 }
